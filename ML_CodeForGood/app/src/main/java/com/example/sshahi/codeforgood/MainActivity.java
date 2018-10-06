05ce8b3b137956b9ca11ca3d56f9960e03540083
Sandeep shahi <shahi.sandeep3@gmail.com>
2018-10-06 00:32:17 -0500

    Microsoft Cognitive Image Classifier added

--- ML_CodeForGood/app/src/main/java/com/example/sshahi/codeforgood/MainActivity.java
+++ ML_CodeForGood/app/src/main/java/com/example/sshahi/codeforgood/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.sshahi.codeforgood;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -14,6 +16,7 @@ import com.google.gson.Gson;
 import com.microsoft.projectoxford.vision.VisionServiceClient;
 import com.microsoft.projectoxford.vision.VisionServiceRestClient;
 import com.microsoft.projectoxford.vision.contract.AnalysisResult;
+import com.microsoft.projectoxford.vision.contract.Caption;
 import com.microsoft.projectoxford.vision.rest.VisionServiceException;
 
 import java.io.ByteArrayInputStream;
@@ -23,25 +26,27 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 public VisionServiceClient visionServiceClient = new VisionServiceRestClient("a7e12e49f690477f8ef9cfa1f1865ab0");
+   ProgressDialog mDialog = new ProgressDialog(MainActivity.this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Bitmap mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cute_girl);
         ImageView imageView = (ImageView)findViewById(R.id.imageView);
-        TextView txtView = (TextView)findViewById(R.id.txtDescription);
         Button btnProcess = (Button)findViewById(R.id.btnProcess);
 
         imageView.setImageBitmap(mBitmap);
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        final ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+
         btnProcess.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                AsyncTask<InputStream, String , String> visionTask = new AsyncTask<InputStream, String, String>() {
+                @SuppressLint("StaticFieldLeak") final AsyncTask<InputStream, String , String> visionTask = new AsyncTask<InputStream, String, String>() {
                     @Override
                     protected String doInBackground(InputStream... params) {
                          try{
@@ -52,15 +57,47 @@ public VisionServiceClient visionServiceClient = new VisionServiceRestClient("a7
 
                              String strResult = new Gson().toJson(result);
                              return strResult;
-                         } catch (IOException e) {
+
+                         } catch (Exception e) {
                               return null;
 
                          }
 
+                        }
+
+                    @Override
+                    public void onPreExecute(){
+                        mDialog.show();
                     }
+                    @Override
+                    public void onPostExecute(String s){
+                        mDialog.dismiss();
+
+                        AnalysisResult result = new Gson().fromJson(s, AnalysisResult.class);
+                        TextView textView = (TextView)findViewById(R.id.txtDescription);
+                        StringBuilder stringBuilder = new StringBuilder();
+                         for (Caption caption: result.description.captions)
+                         {
+                             stringBuilder.append(caption.text);
+                         }
+                         textView.setText(stringBuilder);
+
+                    }
+
+                    @Override
+                    public void onProgressUpdate(String... values){
+
+                        mDialog.setMessage(values[0 ]);
+                    }
+
+
+
                 };
+
+                visionTask.execute(inputStream);
             }
         });
 
     }
 }
+ 
